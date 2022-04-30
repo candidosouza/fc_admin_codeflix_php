3d6decd0426d47c193b9e256b008f59aba87b65d
candidosouza <candidosouzza@gmail.com>
2022-04-30 11:42:05 -0300

    :sparkles: Criação da entidade Category

---
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\Domain\Entity;
+
+use Core\Domain\Entity\Category;
+use PHPUnit\Framework\TestCase;
+
+class CategoryUnitTest extends TestCase
+{
+    public function testAttributes()
+    {
+        $category = new Category(
+            id: 'asdf',
+            name: 'New Cat',
+            description: 'New desc',
+            isActive: true
+        );
+        $this->assertEquals('New Cat', $category->name);
+        $this->assertEquals('New desc', $category->description);
+        $this->assertEquals(true, $category->isActive);
+    }
+}
