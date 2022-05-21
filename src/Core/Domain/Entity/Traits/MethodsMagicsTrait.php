9e7af5d3a758a91a9fa330e79b7c88c464224b00
candidosouza <candidosouzza@gmail.com>
2022-05-21 10:08:44 -0300

    :sparkles: Configurando os métodos magicos nas trait

--- src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
+++ src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
@@ -2,13 +2,26 @@
 
 namespace Core\Domain\Entity\Traits;
 
+use Exception;
+
 trait MethodsMagicsTrait
 {
     public function __get($property)
     {
-        if ($this->{$property})
+        if (isset($this->{$property}))
             return $this->{$property};
+
         $className = get_class($this);
-        throw new \Exception("Property {$property} not found in class {$className}");
+        throw new Exception("Property {$property} not found in class {$className}");
+    }
+
+    public function id(): string
+    {
+        return (string) $this->id;
+    }
+
+    public function createdAt(): string
+    {
+        return $this->createdAt->format('Y-m-d H:i:s');
     }
 }
