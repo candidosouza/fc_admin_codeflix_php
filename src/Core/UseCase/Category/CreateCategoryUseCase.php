3232da2ed6ae14a6809a5943697acbca8379dfdf
candidosouza <candidosouzza@gmail.com>
2022-05-23 08:46:59 -0300

    :sparkles: Configuração do usecase com os DTO's de entrada e saida do cadastro de categorias

--- src/Core/UseCase/Category/CreateCategoryUseCase.php
+++ src/Core/UseCase/Category/CreateCategoryUseCase.php
@@ -4,6 +4,10 @@ namespace Core\UseCase\Category;
 
 use Core\Domain\Entity\Category;
 use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\DTO\Category\CreateCategory\{
+    CategoryCreateInputDto,
+    CategoryCreateOutputDto
+};
 
 class CreateCategoryUseCase
 {
@@ -14,11 +18,19 @@ class CreateCategoryUseCase
         $this->repository = $repository;
     }
 
-    public function execute()
+    public function execute(CategoryCreateInputDto $input): CategoryCreateOutputDto
     {
         $category = new Category(
-            name: 'teste'
+            name: $input->name,
+            description: $input->description,
+            isActive: $input->isActive
         );
-        $this->repository->insert($category);
+        $newCategory = $this->repository->insert($category);
+        return new CategoryCreateOutputDto(
+            id: $newCategory->id(),
+            name: $newCategory->name,
+            description: $newCategory->description,
+            is_active: $newCategory->isActive,
+        );
     }
 }
